0a4b736e60b3a6b13a78cbf1104648401abf3fcd
Dhillon66 <gurkamaldhillon45678@gmail.com>
2024-11-07 05:40:35 +0530

    addition of files

---
+++ javascript.js
@@ -0,0 +1,27 @@
+sheep.addEventListener('click',function(){
+	document.getElementById("image").src="images/sheep.webp";
+   
+    document.getElementById("sheep").className='hh';
+    
+  
+});
+bear.addEventListener('click',function(){
+	document.getElementById("image").src="images/bear.webp";
+   
+    document.getElementById("bear").className='hh';
+});
+moose.addEventListener('click',function(){
+	document.getElementById("image").src="images/moose.webp";
+   
+    document.getElementById("moose").className='hh';
+});
+wolf.addEventListener('click',function(){
+	document.getElementById("image").src="images/wolf.webp";
+   
+    document.getElementById("wolf").className='hh';
+});
+elk.addEventListener('click',function(){
+	document.getElementById("image").src="images/elk.webp";
+   
+    document.getElementById("elk").className='hh';
+});
